121c6c686214950ab9bd00a4e6ba328938b6b322
AxsuAdvi <anna2durai>
2023-10-31 22:53:45 +0530

    Pizza clone changes - map

--- 01-pure-react/pizza-test/src/index.js
+++ 01-pure-react/pizza-test/src/index.js
@@ -8,7 +8,7 @@ const pizzaData = [
       ingredients: "Bread with italian olive oil and rosemary",
       price: 6,
       photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
+      soldOut: true,
     },
     {
       name: "Pizza Margherita",
@@ -60,22 +60,56 @@ function App(){
 function Menu(){
     return <main className="menu">
         <h2>Our menu</h2>
-        <Pizza name="Pizza Spinici"
+        <ul className="pizzas">
+        {/* {pizzaData.map(pizza => <Pizza name={pizza.name} 
+                                      ingredients={pizza.ingredients}
+                                      photoName={pizza.photoName}
+                                      price={pizza.price}
+                                      soldOut={pizza.soldOut}
+                                      />
+                          )} */}
+                          {/* Below foreach not worked */}
+          {/* {pizzaData.forEach(function(value,key){<Pizza pizzaObj={value} key={value.name}/>})}
+          The forEach() method does not returns a new array based on the given array.
+          */}
+         {pizzaData.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name}/> )
+                          )}
+
+
+        </ul >
+        {/* <Pizza name="Pizza Spinici"
                 ingredients="Tomato, potato"
-                photoName="pizzas/spinaci.jpg"/>
+                photoName="pizzas/spinaci.jpg"
+                price={10}/>
         <Pizza name="Pizza Funghi"
                 ingredients="Tomato, Chilli"
-                photoName="pizzas/funghi.jpg"/>
+                photoName="pizzas/funghi.jpg"
+                price={11}/> */}
         
     </main>
 }
 
 function Pizza(props){
-    return <div>
-            <img src={props.photoName} alt="props.name"/>
+    return <li>
+            {/* <img src={props.photoName} alt="props.name"/>
             <h3>{props.name}</h3>
             <span>{props.ingredients}</span>
-        </div>
+            <div>
+            <span>$ {props.price + 8}</span>
+            </div>
+            <div>
+            <span> {props.soldOut}</span>
+            </div> */}
+            <img src={props.pizzaObj.photoName} alt="props.name"/>
+            <div>
+            <h3>{props.pizzaObj.name}</h3>
+            <p>{props.pizzaObj.ingredients}</p>
+            
+            <span>$ {props.pizzaObj.price + 8}</span>
+            
+            <span> {props.pizzaObj.soldOut}</span>
+            </div>
+        </li>
 }
 function Footer(){
     const hour = new Date().getHours();
@@ -96,11 +130,10 @@ function Footer(){
 function Header(){
     // const style = {color:"red", fontSize: "32px"}
     const style = {}
-    return <header className="header"><h1 style={style} >test</h1></header>
+    return <header className="header"><h1 style={style} >Moraccan Pizza</h1></header>
 }
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<React.StrictMode>
-    
+root.render(<React.StrictMode>    
     <App/>
 </React.StrictMode>
 )
