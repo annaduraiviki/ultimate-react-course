d2a762a6a133530df8d3500d68c501b12f1c8eb4
AxsuAdvi <anna2durai>
2023-11-01 23:32:56 +0530

    steps - useState - close button

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -11,7 +11,7 @@ const messages = [
 export default function App(){
   const [step, setStep] = useState(1)
   const [objname, setName] = useState({name:'Anna'})
-
+  const [isOpen, setIsOpen] = useState(true)
   function handlePrevious(){
     if(step > 1)
     setStep(step-1)
@@ -26,19 +26,25 @@ export default function App(){
     setName({name:'Advi'})
   }
   return (
-  <div className="steps">
-    <div className="numbers">
+    <div>
+      <button  className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+
+    {isOpen &&
+      <div className="steps">
+      <div className="numbers">
       <div className={`${step >= 1 ? "active" : ""}`}>1</div>
       <div className={`${step >= 2 ? "active" : ""}`}>2</div>
       <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-    </div>
-    <p className="message">Step {step}:{messages[step -1]} {objname.name}</p>
-    <div className="buttons">
+      </div>
+      <p className="message">Step {step}:{messages[step -1]} {objname.name}</p>
+      <div className="buttons">
       <button style={{backgroundColor: 'purple', color: "#fff"}} onClick={handlePrevious}>previous</button>
       <button style={{backgroundColor: 'purple', color: "#fff"}} onClick={handleNext}>next</button>
+      </div>
+      </div>
+    }
     </div>
-  </div>
-  )
+      )
 }
 
  
